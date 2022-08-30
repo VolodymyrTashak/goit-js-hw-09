43bf2261f0b166dc9c3fbe536b5a19c841b7a88b
VolodymyrTashak <vovavozny33@gmail.com>
2022-08-30 16:44:33 +0300

    3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,43 +5,51 @@ const refs = {
   delay: document.querySelector('[name="delay"]'),
   step: document.querySelector('[name="step"]'),
   amount: document.querySelector('[name="amount"]'),
-  btnSubmit: document.querySelector('[name="submit"]'),
+  btnSubmit: document.querySelector('[type="submit"]'),
 };
+
 refs.form.addEventListener('submit', onSubmit);
-refs.form.addEventListener('intup', onInput);
+refs.form.addEventListener('input', onInput);
 
 let formData = {};
-// let counterPosition = 0;
+let positionCounter = 0;
 
-// function onSubmit(event) {
-//   event.preventDefault();
-//   refs.btnSubmit.disabled = true;
-//   setTimeout(() => {
-//     let counterDelay = Number(formData.delay);
-//     console.log(formData.delay);
-//   });
-// }
-function onSubmit(event) {}
-function onInput(event) {
-  formData[event.target.value] = event.target.value;
-  console.log(event.target.value);
+function onSubmit(evt) {
+  evt.preventDefault();
+  refs.btnSubmit.disabled = true;
+  setTimeout(() => {
+    let counterDelay = Number(formData.delay);
+    let intervalId = null;
+    intervalId = setInterval(() => {
+      if (positionCounter !== Number(formData.amount)) {
+        positionCounter += 1;
+        counterDelay += Number(formData.step);
+        return createPromise(positionCounter, counterDelay)
+          .then(({ position, delay }) => {
+            Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          })
+          .catch(({ position, delay }) => {
+            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          });
+      }
+      clearInterval(intervalId);
+      refs.btnSubmit.disabled = false;
+      positionCounter = 0;
+    }, formData.step);
+  }, formData.delay);
 }
-
-// return createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notify.error(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+function onInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+
     if (shouldResolve) {
       resolve({ position, delay });
     } else {
-      reject({ position, reject });
+      reject({ position, delay });
     }
   });
 }
